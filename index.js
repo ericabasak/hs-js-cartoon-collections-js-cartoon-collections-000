c6c4dc1a592e169d3003e9a97c463bad1a9c0773
Erica <ericabasak@gmail.com>
2019-08-19 17:44:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
+
+
 function dwarfRollCall(dwarves) {
+  var dwarvesArray = [];
+  
+  for (let i = 0; i < dwarves.length; i++) {
+    dwarvesArray.push(`${i + 1}. ${dwarves[i]}`)
+    }
+    return `${dwarvesArray.join(" ")} `;
 }
+
+
+
 
 function summonCaptainPlanet(planeteerCalls){
+  let captainArray = [];
+  
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    captainArray.push(`${planeteerCalls[i].toUpperCase()}!`);
+  }
+  return captainArray;
 }
+
+
+
+
 
 function longPlaneteerCalls(words) {
+  let planeteerArray = false;
+
+    for (let i = 0; i < words.length; i++) {
+        if (planeteerArray = true && words[i].length > 4) {
+        }
+    }
+    return planeteerArray;
 }
 
+
+
+
+
+
 function findTheCheese (foods) {
+ let cheese = ["cheddar", "gouda", "camembert"];
+ 
+ for (let i = 0; i < foods.length; i++) {
+   for (let j = 0; j < cheese.length; j++) {
+     if (foods[i] === cheese[j]) {
+     return foods[i];
+     }
+   }
+ }
+ return "no cheese!";
 }
